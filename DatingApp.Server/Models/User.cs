9cbbe37042b3d63b7361c9ebaf2431636dfeca5a
Nikola Gaic <nikolagaic@yahoo.com>
2024-10-30 16:18:17 +0100

    User columns made nullable (used for updated Register form)

--- DatingApp.Server/Models/User.cs
+++ DatingApp.Server/Models/User.cs
@@ -13,9 +13,9 @@ namespace DatingApp.Server.Models
         public DateTime DateOfBirth { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
-        public string Introduction { get; set; }
-        public string LookingFor { get; set; }
-        public string Interests { get; set; }
+        public string? Introduction { get; set; }
+        public string? LookingFor { get; set; }
+        public string? Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
