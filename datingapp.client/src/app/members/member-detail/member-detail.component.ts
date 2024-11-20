e29237eb3f39c8936bcbea1b4d6e634729c14f2d
Nikola Gaic <nikolagaic@yahoo.com>
2024-11-20 16:52:11 +0100

    Added query params to Angular route to resolve redirection to users message tab from all messages list

--- datingapp.client/src/app/members/member-detail/member-detail.component.ts
+++ datingapp.client/src/app/members/member-detail/member-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { User } from '../../_models/user';
 import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
-import { GalleriaModule } from 'primeng/galleria';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
+import { AuthService } from '../../_services/auth.service';
 
 
 @Component({
@@ -11,12 +12,15 @@ import { GalleriaModule } from 'primeng/galleria';
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.css']
 })
-export class MemberDetailComponent implements OnInit {
+export class MemberDetailComponent implements OnInit, AfterViewInit {
+  @ViewChild('memberTabs', { static: false }) memberTabs?: TabsetComponent;
   user?: User;
   galleryOptions?: any[];
   galleryImages?: any[];
   
-  constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
+  constructor(private userService: UserService, private alertify: AlertifyService, 
+              private route: ActivatedRoute, private cdRef: ChangeDetectorRef, 
+              private authService: AuthService) { }
 
   ngOnInit() {    //now we get the data before activating the route itself
     this.route.data.subscribe(data => {
@@ -38,6 +42,16 @@ export class MemberDetailComponent implements OnInit {
     this.galleryImages = this.getImages();
   }
 
+  ngAfterViewInit() { 
+    this.route.queryParams.subscribe(params => { 
+      const selectedTab = params['tab']; 
+        if (this.memberTabs) { 
+          this.memberTabs.tabs[selectedTab > 0 ? selectedTab : 0].active = true; 
+          this.cdRef.detectChanges(); // Manually trigger change detection
+        } 
+      }); 
+    }
+
   getImages() {
     const imageUrls = [];
     if (this.user?.photos?.length) {
@@ -51,5 +65,28 @@ export class MemberDetailComponent implements OnInit {
       }
     }
     return imageUrls;
+  }
+
+  selectTab(tabId: number) {
+    if (this.memberTabs?.tabs[tabId]) {
+      this.memberTabs.tabs[tabId].active = true;
+      this.cdRef.detectChanges(); 
+    }
+  }
+
+  sendLike(recipientId: number) {
+    if (recipientId !== undefined) {
+        this.userService.sendLike(this.authService.decodedToken.nameid, recipientId).subscribe(
+            data => {
+                this.alertify.success('You have liked: ' + this.user?.knownAs);
+            },
+            error => {
+              const errorMessage = error.message || 'An error occurred'; 
+              this.alertify.error(errorMessage);
+            }
+        );
+    } else {
+        this.alertify.error('User ID is undefined.');
+    }
   }
 }
